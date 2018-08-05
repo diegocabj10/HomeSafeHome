425eb7651d677267363743ffdfc5ff522a98c5d9
Diego Campos <10diegocabj@gmail.com>
2018-08-05 14:51:18 -0300

    Controllers Dispositivos y Seniales con sus pkg

---
+++ Código/Web/Web/Controllers/DispositivosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+using DTO;
+namespace Web.Controllers
+{
+    public class DispositivosController : BaseController<DtoAbmBase, rnAbmBase<DtoAbmBase>>
+    {
+        public DispositivosController()
+        {
+            base.Setup(Entidad: "Dispositivos", SetIdNumerico: "", SetIdNombreIdTipo: "");
+        }
+    }
+}
