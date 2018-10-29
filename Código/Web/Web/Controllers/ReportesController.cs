39a20ddb8a859b78ef65523d61d850edb60c1a79
Diego Campos <10diegocabj@gmail.com>
2018-10-29 20:55:46 -0300

    Reportes back end

---
+++ Código/Web/Web/Controllers/ReportesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+using DTO;
+namespace Web.Controllers
+{
+    public class ReportesController : BaseController<DtoReclamo, rnAbmBase<DtoReclamo>>
+    {
+        public ReportesController()
+        {
+            base.Setup(Entidad: "Reclamos", SetIdNumerico: "", SetIdNombreIdTipo: "");
+        }
+    }
+}
