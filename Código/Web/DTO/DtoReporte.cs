70effcb3ac29fdc0d20ea2a22b15ee0a50f58233
Diego Campos <10diegocabj@gmail.com>
2018-10-29 23:13:00 -0300

    Controller Reclamos

--- Código/Web/DTO/DtoReporte.cs
+++ Código/Web/DTO/DtoReporte.cs
@@ -8,6 +8,9 @@ namespace DTO
 {
     public class DtoReporte : DtoAbmBase
     {
+        public string Senial { get; set; }
         public int IdUsuario { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime Ultimo { get; set; }
     }
 }
