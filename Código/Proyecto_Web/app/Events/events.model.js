22cdb7f7b8ed68700ecd6071b19d63b35dc55dce
Diego <10diegocabj@gmail.com>
2020-11-03 00:43:11 -0300

    Adding new models

--- Código/Proyecto_Web/app/Events/events.model.js
+++ Código/Proyecto_Web/app/Events/events.model.js
@@ -1,12 +1,17 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../../config/db.config');
 const modelDevice = require('../Devices/devices.model');
+const modelSignal = require('../Signals/signals.model');
 const Event = dbConfig.define('T_EVENTOS', {
   FECHA_EVENTO: {
     type: Sequelize.DATE
   },
   ID_SENIAL: {
     type: Sequelize.INTEGER,
+    references: {
+      model: modelSignal,
+      key: 'id'
+    },
     allowNull: false
   },
   ID_DISPOSITIVO: {
